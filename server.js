b9a2263aac0d071592c75454a8a08fcd67d47fa1
EvgeniiShevchenko <evgenii.shevchenko@outlook.com>
2019-06-08 02:32:26 +0300

    version 1.0.22

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get("/bay", function (req, res) {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "client", "build")));
-    app.get("*", function (req, res) {
+    app.get("/*", function (req, res) {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
