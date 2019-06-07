04e04d172ab6a28a7b4b213c4a1e2554382dc59e
EvgeniiShevchenko <evgenii.shevchenko@outlook.com>
2019-06-07 13:24:08 +0300

    version 1.0.16

--- client/src/store/redusers/body/actions.js
+++ client/src/store/redusers/body/actions.js
@@ -8,7 +8,7 @@ export const UPDATE_ANIME = "UPDATE_ANIME";
 
 export const postanime = () => {
     return (dispatch) => {
-        fetch('/start/')
+        fetch('/api/start')
             .then(res => res.json())
             .then(posts => {
                 console.log(JSON.stringify(posts));
