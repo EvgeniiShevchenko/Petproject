b481b5e0f5a85ef30165259eff8a4fbf020c62bd
EvgeniiShevchenko <evgenii.shevchenko@outlook.com>
2019-06-08 14:59:10 +0300

    version 1.0.22

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var _bodyParser = require("body-parser");
 
 var _bodyParser2 = _interopRequireDefault(_bodyParser);
 
+var _cors = require("cors");
+
+var _cors2 = _interopRequireDefault(_cors);
+
 var _os = require("os");
 
 var _morgan = require("morgan");
@@ -43,6 +47,7 @@ if (!dev) {
 
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use((0, _cors2.default)());
 app.use((0, _morgan2.default)('dev'));
 
 app.get('/api', function (req, res) {
